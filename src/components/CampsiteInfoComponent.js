89bc3358049d9d178f57d1b3d713be6fa8fd3ef6
Albert Vazquez <avazquez25@gmail.com>
2020-03-26 19:30:23 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
 
     renderCampsite(campsite) {
         return (
@@ -44,9 +39,11 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
